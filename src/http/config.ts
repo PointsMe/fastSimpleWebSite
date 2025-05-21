6b4458cee576c5781bebb651630932578a745f5a
Msoepa0528 <hongchangliang8@gmail.com>
2025-05-21 17:29:49 +0800

    update version

--- src/http/config.ts
+++ src/http/config.ts
@@ -24,10 +24,16 @@ export const languageList = [
     //     label: 'Català'
     // }
 ]
+/**
+ * 热敏打印机写死id，需单独展示
+ */
 export const hotGoodsId = {
         id: '100030101',
         name: '热敏打印机',
 }
+/**
+ * pos机写死id，需单独展示
+ */
 export const posGoodsId = {
     id: '100070101',
     name: 'POS机器PAXA920PRO 4G无线网络WIFI+4G',
