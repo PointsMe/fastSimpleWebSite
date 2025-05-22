b6e2a5d5d63a545240e00d2c0db14b5baf1eb59a
Msoepa0528 <hongchangliang8@gmail.com>
2025-05-22 15:33:47 +0800

    易酷首页

--- src/views/homeComponents/BannerFour.vue
+++ src/views/homeComponents/BannerFour.vue
@@ -1,14 +1,76 @@
 <template>
   <div class="banner-4">
-    <img src="@/assets/banner-4-1.png" alt="" />
+    <el-row>
+      <el-col :span="12">
+        <div class="left">
+        <p>
+          新功能AI语音生成订单
+        </p>
+        <p>
+          三种便捷方式，适用于各种场景
+        </p>
+        <img src="@/assets/joinkuImages/b-4-1.png" alt="">
+        </div>
+      </el-col>
+      <el-col :span="12">
+        <div class="right">
+           <div class="right-one">
+             <el-row>
+              <el-col :span="12">
+                <div class="right-one-left">
+                  智能订单生成，提升运营效率
+                </div>
+              </el-col>
+              <el-col :span="12">
+                <div class="right-one-right">
+                  01
+                </div>
+              </el-col>
+             </el-row>
+           </div>
+        </div>
+      </el-col>
+    </el-row>
   </div>
 </template>
 <style scoped lang="less">
 .banner-4 {
   width: 1280px;
   margin: auto;
-  img {
-    width: 100%;
+  p{
+    margin: 0;
+    padding: 0;
+  }
+  .right{
+    padding: 20px 0 20px 20px;
+    border-left: 1px solid #E5E5E5;
+    .right-one{
+      .right-one-left{
+        font-family: Source Han Sans SC, Source Han Sans SC;
+font-weight: 400;
+font-size: 18px;
+      }
+    }
+  }
+  .left {
+    font-family: Source Han Sans SC, Source Han Sans SC;
+    p:first-child {
+font-weight: bold;
+font-size: 52px;
+color: #1B1B1B;
+    }
+    p:last-child {
+
+font-weight: 400;
+font-size: 18px;
+color: #1B1B1B;
+    }
+    img{
+      width: 340px;
+      display: block;
+      margin: auto;
+      margin-top: 60px;
+    }
   }
 }
 </style>
--- src/views/homeComponents/BannerThree.vue
+++ src/views/homeComponents/BannerThree.vue
@@ -3,17 +3,77 @@
     <div class="content">
       <div class="title">
         <el-row class="row-t-5">
-          <el-col :span="10" class="col-1-t"> 两大专业仓库管理系统 </el-col>
-          <el-col :span="4" class="col-2-t">
+          <el-col :span="24" class="col-1-t"> 两大专业仓库管理系统 </el-col>
+          <!-- <el-col :span="4" class="col-2-t">
             <div class="line"></div>
-          </el-col>
-          <el-col :span="10" class="col-3-t">
+          </el-col> -->
+          <el-col :span="24" class="col-3-t">
             满足各类商户需求，确保百货机食品供货仓储高效、规范运营
           </el-col>
         </el-row>
       </div>
       <div class="center">
-        <img src="@/assets/banner3-1.png" alt="" />
+        <!-- <img src="@/assets/banner3-1.png" alt="" /> -->
+        <el-row>
+          <el-col :span="12">
+            <div class="content-3-left">
+              <div class="content-3-left-item">
+                <img src="@/assets/joinkuImages/b-3-1.png" alt="" />
+                <span>意酷供应商</span>
+              </div>
+            </div>
+            <el-row class="row-center-item">
+              <el-col :span="3">
+                <div>
+                  <img src="@/assets/joinkuImages/b-3-3.png" alt="" />
+                </div>
+              </el-col>
+              <el-col :span="21">
+                <div class="sub-title">
+                  <p>AI商品采购智能管理</p>
+                  <p>
+                    依托历史订单、库存周转与市场趋势，自动生成精准采购建议，助力供应商优化进货节奏，平衡供需结构，提升整体运营效率。
+                  </p>
+                </div>
+              </el-col>
+            </el-row>
+            <el-row class="row-center-item">
+              <el-col :span="3">
+                <div>
+                  <img src="@/assets/joinkuImages/b-3-3.png" alt="" />
+                </div>
+              </el-col>
+              <el-col :span="21">
+                <div class="sub-title">
+                  <p>AI智能销售预售单</p>
+                  <p>
+                    通过对下游客户采购模式的深度学习，预判未来需求，自动生成销售预订单，实现从被动接单到主动供给的转变，增强客户黏性与复购率。
+                  </p>
+                </div>
+              </el-col>
+            </el-row>
+            <el-row class="row-center-item">
+              <el-col :span="3">
+                <div>
+                  <img src="@/assets/joinkuImages/b-3-3.png" alt="" />
+                </div>
+              </el-col>
+              <el-col :span="21">
+                <div class="sub-title">
+                  <p>AI智能配送路线生成</p>
+                  <p>
+                    系统融合地理定位、订单优先级与物流资源，智能规划最优配送路径，自动排单、同步发货状态，全面提升履约效率与客户满意度。
+                  </p>
+                </div>
+              </el-col>
+            </el-row>
+          </el-col>
+          <el-col :span="12">
+            <div class="right-img">
+              <img src="@/assets/joinkuImages/banner-3-div.png" alt="" />
+            </div>
+          </el-col>
+        </el-row>
       </div>
       <div class="bottom-banner-3">
         <el-row :gutter="20">
@@ -33,7 +93,9 @@
           <el-col :span="7" class="col-b border-top">
             <div class="con-a-b">
               <div class="t-b">智能库存管理</div>
-              <div class="t-b-t">以批次级精度跟踪库存，以确保准确性可可追溯性。 高效的订单处理</div>
+              <div class="t-b-t">
+                以批次级精度跟踪库存，以确保准确性可可追溯性。 高效的订单处理
+              </div>
             </div>
           </el-col>
           <el-col :span="1"></el-col>
@@ -76,7 +138,67 @@
       </div>
 
       <div class="center">
-        <img src="@/assets/banner3-2.png" alt="" />
+        <!-- <img src="@/assets/banner3-1.png" alt="" /> -->
+        <el-row>
+          <el-col :span="12">
+            <div class="content-3-left">
+              <div class="content-3-left-item">
+                <img src="@/assets/joinkuImages/b-3-2.png" alt="" />
+                <span style="color: #387a46;">意酷</span>
+              </div>
+            </div>
+            <el-row class="row-center-item">
+              <el-col :span="3">
+                <div>
+                  <img src="@/assets/joinkuImages/b-3-3.png" alt="" />
+                </div>
+              </el-col>
+              <el-col :span="21">
+                <div class="sub-title">
+                  <p>SmartOps中枢</p>
+                  <p>
+                    (Smart Operations)作为 Joinku Retail 的核心引擎，SmartOps 可自动学习门店销售节奏，动态优化商品结构、进货频率、定价策略与促销逻辑，为商户提供全周期的智能经营支持
+                  </p>
+                </div>
+              </el-col>
+            </el-row>
+            <el-row class="row-center-item">
+              <el-col :span="3">
+                <div>
+                  <img src="@/assets/joinkuImages/b-3-4.png" alt="" />
+                </div>
+              </el-col>
+              <el-col :span="21">
+                <div class="sub-title">
+                  <p>MerchAI智能商品助手</p>
+                  <p>
+                    (Merchandise)MerchAI 可基于实时数据流，自动判别商品热度、库存周转与销售波动，辅助商户制定科学选品、精准补货及利润最大化的销售方案
+                  </p>
+                </div>
+              </el-col>
+            </el-row>
+            <el-row class="row-center-item">
+              <el-col :span="3">
+                <div>
+                  <img src="@/assets/joinkuImages/b-3-5.png" alt="" />
+                </div>
+              </el-col>
+              <el-col :span="21">
+                <div class="sub-title">
+                  <p>Retail Brain零售决策引擎</p>
+                  <p>
+                    Retail Brain集成AI算法与经营模型，实时生成商品运营建议，持续优化商品生命周期价值，让每个SKU（Stock Keeping Unit 库存单位）都成为利润单元
+                  </p>
+                </div>
+              </el-col>
+            </el-row>
+          </el-col>
+          <el-col :span="12">
+            <div class="right-img">
+              <img src="@/assets/joinkuImages/b-3-6.png" alt="" />
+            </div>
+          </el-col>
+        </el-row>
       </div>
       <div class="bottom-banner-3">
         <el-row :gutter="20">
@@ -142,7 +264,10 @@
 .warehouse-management {
   width: 100%;
   background-color: #fff;
-
+  p {
+    margin: 0;
+    padding: 0;
+  }
   .content {
     width: 1280px;
     margin: auto;
@@ -189,7 +314,7 @@
             font-size: 32px;
             height: 36px;
             line-height: 36px;
-
+            margin-bottom: 10px;
             .tips {
               display: inline-block;
               margin-left: 15px;
@@ -216,22 +341,71 @@
     }
 
     .center {
-      width: 100%;
-
-      > img {
-        width: 100%;
+      width: 85%;
+      margin: auto;
+      .el-col {
+        // display: flex;
+        // flex-direction: column;
+        // justify-content: space-between;
+      }
+      .right-img {
+        height: 100%;
+        margin: auto;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+      }
+      .row-center-item {
+        margin-top: 40px;
+        img {
+          width: 52px;
+          height: 52px;
+        }
+        .sub-title {
+          font-family: Source Han Sans SC, Source Han Sans SC;
+          p:first-child {
+            font-weight: bold;
+            font-size: 24px;
+            color: #000000;
+          }
+          p:nth-child(2) {
+            font-weight: 400;
+            font-size: 16px;
+            color: #595959;
+          }
+        }
+      }
+      .content-3-left {
+        margin-bottom: 20px;
+        margin-top: 50px;
+        .content-3-left-item {
+          display: flex;
+          align-items: center;
+          justify-content: left;
+          > img {
+            width: 60px;
+            height: 60px;
+          }
+          > span {
+            font-family: Source Han Sans SC, Source Han Sans SC;
+            font-weight: bold;
+            font-size: 45px;
+            color: #365aa1;
+            margin-left: 10px;
+          }
+        }
       }
     }
 
     .title {
-      margin-top: 150px;
+      margin-top: 100px;
       text-align: left;
       margin-bottom: 20px;
       .row-t-5 {
-        height: 70px;
-        line-height: 70px;
+        // height: 70px;
+        // line-height: 70px;
         .col-3-t {
-          text-indent: 20px;
+          // text-indent: 20px;
           font-weight: 400;
           font-size: 18px;
         }
